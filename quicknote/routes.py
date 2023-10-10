805593875a2545b12dc04ec3c46c3d8bee7ff40d
ThomasBallardCI <ThomasGEBallard@gmail.com>
2023-10-10 13:41:36 +0000

    basic routing of pages and base html created/rendering

--- quicknote/routes.py
+++ quicknote/routes.py
@@ -5,4 +5,34 @@ from quicknote.models import User, Note
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("home.html")
+
+
+@app.route("/notes")
+def notes():
+    return render_template("notes.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+    # if request.method == "POST":
+    #     email = request.form.get("email")
+    #     password = request.form.get("password")
+
+    #     user - User.query.filter_by(email=email).first()
+    #     if user:
+    #         if check_password_hash(user.password, password):
+    #             flash("Logged in Successfully!", category="success")
+    #         else:
+    #             flash("Incorrect Password, Try again.")
+
+
+@app.route("/logout")
+def logout():
+    return render_template("logout.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
