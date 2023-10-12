84edea3ca300b494b05a250f9be8fe5b8044722b
ThomasBallardCI <ThomasGEBallard@gmail.com>
2023-10-12 15:16:59 +0000

    adjusted note model data to content for better understanding and removed title from base.html

--- quicknote/models.py
+++ quicknote/models.py
@@ -17,6 +17,6 @@ class Note(db.Model):
     # schema for Notes model
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150))
-    data = db.Column(db.String(10000))
+    content = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
